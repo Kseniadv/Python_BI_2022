8834ae1ec3b7270a04018a81f93428fa33bb1636
Kseniadv <xeniadracheva@gmail.ru>
2022-10-16 21:46:26 +0300

    edited program code

--- hw2_function/README.md
+++ hw2_function/README.md
@@ -1,9 +1,21 @@
 Program to work with fastq files.
 
+The program filters the reads:
+1. According to GC-content.
+2. According quality.
+3. According length.
+Result will be saved in file.
+
+The program consist from some functions:
+- Function for calculation GC-content
+- Function for calculating average quality
+- Function for checking if a file exists
+- 'Main' function.
+
 The function 'main' takes the following arguments:
-1. input_fastq - путь к файлу, который подаётся на вход программе (обычный не сжатый fastq файл)
-2. output_file_prefix - префикс пути к файлу, в который будет записан результат. 
-3. gc_bounds - интервал GC состава (в процентах) для фильтрации (по-умолчанию равен (0, 100)).
-4. length_bounds - интервал длины для фильтрации (по-умолчанию равен (0, 2**32)).
-5. quality_threshold - пороговое значение среднего качества рида для фильтрации, по-умолчанию равно 0 (шкала phred33).
-6. save_filtered - сохранять ли отфильтрованные риды, по-умолчанию равен False.
+1. input_fastq - the path to the file that is fed to the input of the program (uncompressed fastq file).
+2. output_file_prefix - prefix of the path to the file where the result will be written. 
+3. gc_bounds - interval of GC-content (in percent) for filtering (default is (0, 100)).
+4. length_bounds - length interval for filtering (default is (0, 2**32)).
+5. quality_threshold - average read quality threshold for filtering, default is 0 (phred33 scale).
+6. save_filtered - whether to save filtered reads, default is False.
--- hw2_function/fastq_filtrator.py
+++ hw2_function/fastq_filtrator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[40]:
+# In[51]:
 
 
 #Counting GC content
 def GC_comp(seq):
-    GC = ((seq.upper().count('C') + seq.upper().count('G'))/len(seq))*100
+    GC = ((seq.upper().count('C') + seq.upper().count('G'))/len(seq.strip()))*100
+    GC = round(GC, 2)
     return (GC)
 
 #Counting average quality
@@ -15,44 +16,53 @@ def quality(seq):
     for sym in seq:
         q_score = ord(sym)-33
         av += q_score
-    av = round(av/len(seq), 2)
+    av = round(av/len(seq.strip()), 2)
     return(av)
+
+#Check for file existence 
+def is_accessible(path, mode='r'):
+    try:
+        f = open(path, mode)
+        f.close()
+    except IOError:
+         return False
+    return True
 
 def main (input_fastq, output_file_prefix, gc_bounds = (0, 100), length_bounds = (0, 2**32),
           quality_threshold = 0, save_filtered = False):
     
-    if type(gc_bounds) == int or type(gc_bounds) == float:
+    if type(gc_bounds) != tuple:
         gc_bounds = (0, gc_bounds)
-    if type(length_bounds) == int or type(length_bounds) == float:
+    if type(length_bounds) != tuple:
         length_bounds = (0, length_bounds)
         
     output_passed = output_file_prefix + '_passed.fastq'
+    if is_accessible(output_passed, mode='r') == True:
+        f = open(output_passed, 'w')
+        f.close
+        
     output_failed = output_file_prefix + '_failed.fastq'
+    if is_accessible(output_failed, mode='r') == True:
+        f = open(output_failed, 'w')
+        f.close
     
-    one_read = []
     i = 0
 
     with open (input_fastq) as file:
-        if i != 4:
-            for line in file:         
-                one_read.append(line.strip())
-                i = i + 1  
-                
-        GC = GC_comp(one_read[1])
-        len_rid = len(one_read[1])
-        av_qual = quality(one_read[3])
-        
-        if ((GC >= gc_bounds[0]) & (GC <= gc_bounds[1])) & ((len_rid >= length_bounds[0]) & (len_rid <= length_bounds[1]) 
+        line_count = sum(1 for line in open('test.fastq'))
+        while i != line_count:
+            one_rid = [next(file) for x in range(4)]
+            GC = GC_comp(one_rid[1].strip())
+            len_rid = len(one_rid[1].strip())
+            av_qual = quality(one_rid[3].strip())
+            if ((GC >= gc_bounds[0]) & (GC <= gc_bounds[1])) & ((len_rid >= length_bounds[0]) & (len_rid <= length_bounds[1]) 
                                                             & (av_qual >= quality_threshold)):
-            with open (output_passed, 'a' ) as f:
-                for element in one_read:
-                    f.write(element)
-                    f.write('\n')
-        elif save_filtered == True:
-            with open (output_failed, 'a' ) as f:
-                for element in one_read:
-                    f.write(element)
-                    f.write('\n')
-        i = 0
-        one_read = []
+                with open (output_passed, 'a') as f:
+                    f.writelines(one_rid)
+            elif save_filtered == True:
+                with open (output_failed, 'a' ) as f:
+                    f.writelines(one_rid)            
+            one_rid = []
+            i +=4           
+            
 
